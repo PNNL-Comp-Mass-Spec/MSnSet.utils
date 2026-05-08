7d9f98381939a188a30f8c8362682fa827401df4
Vlad Petyuk <petyuk@gmai.com>
2026-05-07 20:40:59 -0700

    new method for adjusting PTM abundances by global protein

--- R/ptm_adjustment_by_global.R
+++ R/ptm_adjustment_by_global.R
@@ -10,7 +10,33 @@
 #'
 #' @param m_ptm MSnSet with PTM data
 #' @param m_global MSnSet with global data
+#' @param method One of "subtraction", "compensation" or "regression"
 #'
+#' @details
+#' \itemize{
+#'    \item \bold{Subtraction:} This simple method works best if there is
+#'    no dynamic range compression. Otherwise, if global data has higher
+#'    compression, the PTM data will be undercorrected. It calculates
+#'    the adjusted profile as: \eqn{PTM_{adj} = PTM_{obs} - Protein_{global}}.
+#'
+#'    \item \bold{Compensation:} Experimental! The algorithm tries to
+#'    infer the overall dynamic range compression coefficient from the entire
+#'    data by fitting \emph{total} least squares regression. Then this coefficient
+#'    is use for the adjustment similar to the \bold{subtraction} algorithm:
+#'    \eqn{PTM_{adj} = PTM_{obs} - coeff*Protein_{global}}
+#'
+#'    \item \bold{Regression:} This method fits a linear model for each feature:
+#'    \eqn{PTM_{obs} \sim \beta_0 + \beta_1 \cdot Protein_{global}}. Then
+#'    \eqn{PTM_{adj} = PTM_{obs} - \beta_0 + \beta_1 \cdot Protein_{global}}
+#'    Works well only if there are lots of samples with varying change
+#'    (e.g. cohort of human subjects). It probably won't work for A vs B
+#'    comparison. Since this is basically two point and thus there is no
+#'    room for regression.
+#' }
+#'
+#' Features that do not have matching parent protein data or have insufficient
+#' data points for regression are returned as \code{NA} and subsequently
+#' filtered.
 #'
 #' @return MSnSet object
 #' @importFrom Biobase fData featureNames exprs
@@ -19,7 +45,7 @@
 #'
 
 
-adjust_for_parent_protein_profiles <- function(m_ptm, m_global){
+adjust_for_parent_protein_profiles <- function(m_ptm, m_global, method = "subtraction"){
 
    # sample alignment
    common_samples <- intersect(sampleNames(m_ptm), sampleNames(m_global))
@@ -36,22 +62,39 @@ adjust_for_parent_protein_profiles <- function(m_ptm, m_global){
 
    x_ptm <- exprs(m_ptm)
 
-   pf <- function(i){
-      x <- as.numeric(x_global[i,])
-      y <- as.numeric(x_ptm[i,])
-      idx <- !is.na(x) & !is.na(y)
-      if(sum(idx) > 1){
-         mdl <- lm(y ~ x)
-         ynot <- predict(mdl, newdata = data.frame(x))
-         return(y - as.numeric(ynot))
-      }else{
-         return(rep(NA,ncol(x_ptm)))
+   if(method == "subtraction"){
+      x_ptm_adj <- x_ptm - x_global
+   }else if(method == "compensation"){
+      #
+      df <- data.frame(global = as.vector(x_global), ptm = as.vector(x_ptm))
+      df_clean <- na.omit(df[, c("global", "ptm")])
+      pca_mod  <- prcomp(df_clean, scale. = FALSE)
+      loadings <- pca_mod$rotation
+      slope <- loadings["ptm", 1] / loadings["global", 1]
+      # bulletproof
+      slope <- abs(slope) * sign(cor(df$global, df$ptm, use = "complete.obs"))
+      x_ptm_adj <- x_ptm - slope*x_global
+   }else if(method == "regression"){
+      pf <- function(i){
+         x <- as.numeric(x_global[i,])
+         y <- as.numeric(x_ptm[i,])
+         idx <- !is.na(x) & !is.na(y)
+         if(sum(idx) > 1){
+            mdl <- lm(y ~ x)
+            ynot <- predict(mdl, newdata = data.frame(x))
+            return(y - as.numeric(ynot))
+         }else{
+            return(rep(NA,ncol(x_ptm)))
+         }
       }
+
+      res <- lapply(1:nrow(m_ptm), pf)
+
+      x_ptm_adj <- matrix(unlist(res), byrow=TRUE, nrow=nrow(x_ptm))
+   } else{
+      stop("unknown method")
    }
 
-   res <- lapply(1:nrow(m_ptm), pf)
-
-   x_ptm_adj <- matrix(unlist(res), byrow=TRUE, nrow=nrow(x_ptm))
    exprs(m_ptm) <- x_ptm_adj
 
    idx_NAs <- apply(is.na(exprs(m_ptm)), 1, all)
